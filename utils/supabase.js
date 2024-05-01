2216a42029000730c73dccd50a5bb20cce40bc7c
Naman <23036464+naman1-gupta@users.noreply.github.com>
2024-05-02 02:30:36 +0530

    Revoke old exposed key

--- utils/supabase.js
+++ utils/supabase.js
@@ -8,7 +8,7 @@ export const insertData = (data) => {
             },
             {
                 headers: {
-                    "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjM2MzQ0OTU5LCJleHAiOjE5NTE5MjA5NTl9.Yl4gox2_jbH8YUTVfWsoutijzOsE83y7-8pPGt-SdJc",
+                    "apikey": "<revoked>",
                     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjM2MzQ0OTU5LCJleHAiOjE5NTE5MjA5NTl9.Yl4gox2_jbH8YUTVfWsoutijzOsE83y7-8pPGt-SdJc",
                     "Content-Type": "application/json",
                     "Prefer": "return=minimal",
